8f280145516ca4655891129d3b3a7cd73c67913d
shodiboy_o <ozodbekonline1@gmail.com>
2023-09-29 21:20:31 +0500

    Added logout with authenficated using Middleware

--- quiz-project/routes/web.php
+++ quiz-project/routes/web.php
@@ -17,5 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/register',[AuthController::class,'loadRegister']);
-Route::post('/register',[AuthController::class,'studentRegister'])->name('studentRegister');
+Route::get('/register', [AuthController::class, 'loadRegister']);
+Route::post('/register', [AuthController::class, 'studentRegister'])->name('studentRegister');
+
+
+Route::get('/login', function () {
+    return redirect('/');
+});
+Route::get('/', [AuthController::class, 'loadLogin']);
+Route::post('/login', [AuthController::class, 'userLogin'])->name('userLogin');
+Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => ['web', 'checkAdmin']], function () {
+    Route::get('/admin/dashboard', [AuthController::class, 'adminDashboard']);
+});
+
+Route::group(['middleware' => ['web', 'checkStudent']], function () {
+    Route::get('/dashboard', [AuthController::class, 'loadDashboard']);
+});
