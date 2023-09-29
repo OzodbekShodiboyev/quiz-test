87020ddc52b1db1c3aa2f1b250ede74bd22181f5
shodiboy_o <ozodbekonline1@gmail.com>
2023-09-29 18:58:28 +0500

    Added Registration Form

---
+++ quiz-project/app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function loadRegister()
+    {
+        return view('register');
+
+    }
+    public function studentRegister(Request $request)
+    {
+        $request->validate([
+            'name'=>'string|required|min:1',
+            'email'=>'string|email|required|max:100|unique:users',
+            'password'=>'string|required|confirmed|min:6'
+        ]);
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return back()->with('success','Your Registration has been successfull.');
+    }
+}
